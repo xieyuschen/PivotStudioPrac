9a4862c4776bc60d5f3ab57d60d3a7c82854c245
tjapzjj <2038975825@qq.com>
2020-11-05 22:13:01 +0800

    down the part of sending email

--- send.go
+++ send.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/smtp"
 	"os"
 	"regexp"
 	"strings"
@@ -11,7 +12,7 @@ import (
 
 func main() {
 
-	url := "https://www.zhihu.com"
+	url := "https://tophub.today/n/mproPpoq6O"
 	method := "GET"
 
 	client := &http.Client{}
@@ -77,4 +78,27 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	/*写入文件*/
+	identity1 := ""
+	sender1 := "2038975825@qq.com"
+	pwd1 := "dumqnhmcxthbbgfg"
+	host1 := "smtp.qq.com"
+	port1 := "25"
+	sendTo1 := []string{"2038975825@qq.com"}
+	senderName1 := "ZhiHuHot"
+	title1 := "ZhiHuHot"
+	body1 := strings.TrimSpace(src)
+	auth1 := smtp.PlainAuth(identity1, sender1, pwd1, host1)
+	content_type1 := "Content-Type: text/html; charset=UTF-8"
+	msg1 := []byte("To: " + strings.Join(sendTo1, ",") + "\nFrom: " + senderName1 +
+		"<" + sender1 + ">\nSubject: " + title1 + "\n" + content_type1 + "\n" + body1 + "\n")
+
+	url1 := host1 + ":" + port1
+	err1 := smtp.SendMail(url1, auth1, sender1, sendTo1, msg1)
+	if err1 != nil {
+		fmt.Printf("\n\nsend mail error: %v", err)
+		return
+	}
+	fmt.Println("\n\nsend mail success!")
+	/*发送文件*/
 }
